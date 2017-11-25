5cf1375f5b0a694cd6b299e56426918ce7a81cff
samson rapando <samsonrapando@gmail.com>
2017-11-25 12:59:15 +0300

    changed models

--- jobonicUsers/views.py
+++ jobonicUsers/views.py
@@ -7,6 +7,8 @@ from jobonicUsers.models import User, LoginSession
 from jobonicUsers.serializers import UserSerializer, LoginSessionSerializer
 
 from blocks import auth, moments
+from blocks.formatter import pack
+
 
 @csrf_exempt
 def user_list(request):
@@ -17,20 +19,18 @@ def user_list(request):
     if request.method == "GET":
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return JsonResponse(pack(serializer.data), safe=False)
 
     elif request.method == 'POST':
         data = JSONParser().parse(request)
         data['salt'] = auth.generate_str(32)
         data['password'] = auth.hash_password(data['salt'], data['password'])
-        data['created'] = moments.now()
         serializer = UserSerializer(data=data)
-        
+
         if serializer.is_valid():
-           
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status = 400)
+            return JsonResponse(pack(serializer.data, True, "User Updated Successfully"), status=201)
+        return JsonResponse(pack (serializer.errors, False, "Error in data receieved"), status=400)
 
 
 @csrf_exempt
@@ -56,9 +56,9 @@ def user_details(request, pk):
         return JsonResponse(serializer.errors, status=400)
 
     elif request.method == 'DELETE':
-       user.delete()
-       return HttpResponse(status=204)
-			
+        user.delete()
+        return HttpResponse(status=204)
+
 
 @csrf_exempt
 def user_login(request):
@@ -68,14 +68,14 @@ def user_login(request):
             user = User.objects.get(email_address=data['email_address'])
         except User.DoesNotExist:
             return HttpResponse(status=400)
-        
+
         stored_pass = user.password
         stored_salt = user.salt
-        
+
         unhash = auth.unhash_password(stored_salt, data['password'], stored_pass)
         if unhash:
-            new_session = LoginSession(user_id = user, created = moments.now(), expire = moments.now() + 3600)
+            new_session = LoginSession(user_id=user, created=moments.now(), expire=moments.now() + 3600)
             sess = LoginSessionSerializer(new_session)
             return JsonResponse(sess.data, safe=False)
         else:
-            return JsonResponse({"msg" : "Unsuccessfully"})
+            return JsonResponse({"msg": "Unsuccessfully"})
